e0fe4a67a6600d9f91fb35865a267d0ac9f86a63
Francisco Catalán <francisco.catalan213@gmail.com>
2021-10-07 22:15:07 -0300

    Add files via upload

--- R/CxF.R
+++ R/CxF.R
@@ -4,19 +4,19 @@
 #'
 #' @return a CSV file
 #' @export
-#'
+#' @import utils
 #' @examples
 CxF <- function(data){
-  ï..ActiveScreener.Id <- 1; Time.of.Screening <- 1 ; Title <- 1 ; Authors <- 1
+  i..ActiveScreener.Id <- 1; Time.of.Screening <- 1 ; Title <- 1 ; Authors <- 1
   Question <- 1; List.of.Reviewers <- 1; Answers <- 1
-  df <- data.frame(ï..ActiveScreener.Id , Time.of.Screening, Title, Authors, Question, List.of.Reviewers, Answers)
-  rm(ï..ActiveScreener.Id ); rm(Time.of.Screening); rm(Title); rm(Authors); rm(Question); rm(List.of.Reviewers);
+  df <- data.frame(i..ActiveScreener.Id , Time.of.Screening, Title, Authors, Question, List.of.Reviewers, Answers)
+  rm(i..ActiveScreener.Id ); rm(Time.of.Screening); rm(Title); rm(Authors); rm(Question); rm(List.of.Reviewers);
   rm(Answers)
   for(j in 1:nrow(data)){
     for(i in 7:ncol(data)){
       if(!is.na(data[j, i])){
         aux <- c(data[j, 1:6], data[[j, i]])
-        names(aux) <- c("ï..ActiveScreener.Id", "Time.of.Screening", "Title", "Authors",
+        names(aux) <- c("i..ActiveScreener.Id", "Time.of.Screening", "Title", "Authors",
                         "Question", "List.of.Reviewers","Answers")
         df <- rbind(df, aux)
       }
